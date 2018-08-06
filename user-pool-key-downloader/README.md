15cc3a33f66cf9524721b4aaee6bf71f301695e6
Brian Winant <brianwinant@gmail.com>
2018-08-06 18:08:42 +0900

    Use argparse for handling command line arguments and improve README

--- user-pool-key-downloader/README.md
+++ user-pool-key-downloader/README.md
@@ -2,11 +2,29 @@ If you want to decode and validate Cognito User Pool ID and Access tokens yourse
 
 `https://cognito-idp.{region}.amazonaws.com/{userPoolId}/.well-known/jwks.json`
 
-To feed the keys into any crypto library, they need to be converted to a well known format. This script will download the keys from AWS, convert them to PKCS#1 format, then save them to a JSON file. 
+The `jwks.json` file will contain information necessary to reconstruct 2 RSA public keys - one for the Cognito id token and another for the Cognito access token. 
+
+To feed the keys into a crypto library, they need to be converted to a well known format. This script will download the keys from AWS, convert them to PKCS#1 format, then save them to a JSON file in the format:
 
 ```
+{
+    "$accessTokenKey": "$rsaPublicKey",
+    "idTokenKey": "$rsaPublicKey"
+}
+``` 
+
+_Note_: the key order will be indeterminate
+
+To run `download-jwks`:
+```
 npm install
-node download-jwks.js <AWS region> <Cognito User Pool ID> <output file>
+node ./download-jwks.js -r <AWS region> -u <Cognito User Pool ID> -o <output file>
  ``` 
 
-See https://aws.amazon.com/premiumsupport/knowledge-center/decode-verify-cognito-json-token/ for more information
+You can extract and verify the RSA public keys by:
+
+- List the key ids: `cat <output file> | jq -r  'to_entries[] | "\(.key)"'`
+- Verify a RSA public key: `cat <output file> | jq -r -j '.["$keyId"]' | openssl rsa -inform PEM -pubin -text -noout`
+
+
+See https://aws.amazon.com/premiumsupport/knowledge-center/decode-verify-cognito-json-token/ for more information    
--- user-pool-key-downloader/download-jwks.js
+++ user-pool-key-downloader/download-jwks.js
@@ -3,64 +3,65 @@
 const https = require('https');
 const fs = require('fs');
 const jwkToPem = require('jwk-to-pem');
+const ArgumentParser = require('argparse').ArgumentParser;
+
 
 // Downloads specified JSON Web Keys from AWS and converts them to PEM PKCS#1
-const downloadKeys = (region, userPoolId, callback) => {
+const downloadKeys = (region, userPoolId) => {
     const jwksUrl = `https://cognito-idp.${region}.amazonaws.com/${userPoolId}/.well-known/jwks.json`;
     console.log(`Downloading keys from ${jwksUrl}`);
 
-    https
-        .get(jwksUrl, (res) => {
-            if (res.statusCode !== 200) {
-                callback(new Error(res.statusCode))
-            }
+    return new Promise((resolve, reject) => {
+        https
+            .get(jwksUrl, (res) => {
+                if (res.statusCode !== 200) {
+                    reject(new Error(res.statusCode))
+                }
 
-            let buffer = '';
+                let buffer = '';
 
-            res.setEncoding('utf-8');
-            res.on('data', (chunk) => {
-                buffer += chunk;
-            });
-            res.on('end', () => {
-                const json = JSON.parse(buffer);
-                const pems = {};
-
-                // Convert each key to PEM
-                json['keys'].forEach(key => {
-                    const key_id = key.kid;
-                    const modulus = key.n;
-                    const exponent = key.e;
-                    const key_type = key.kty;
-                    const jwk = {kty: key_type, n: modulus, e: exponent};
-
-                    console.log(`Converting ${key_type} key ${key_id}`);
-                    pems[key_id] = jwkToPem(jwk);
+                res.setEncoding('utf-8');
+                res.on('data', (chunk) => {
+                    buffer += chunk;
                 });
+                res.on('end', () => {
+                    const json = JSON.parse(buffer);
+                    const pems = {};
 
-                callback(null, pems);
+                    // Convert each key to PEM
+                    json['keys'].forEach(key => {
+                        const key_id = key.kid;
+                        const modulus = key.n;
+                        const exponent = key.e;
+                        const key_type = key.kty;
+                        const jwk = {kty: key_type, n: modulus, e: exponent};
+
+                        console.log(`Converting ${key_type} key ${key_id}`);
+                        pems[key_id] = jwkToPem(jwk);
+                    });
+
+                    resolve(pems);
+                });
+            })
+            .on('error', (err) => {
+                reject(err);
             });
-        })
-        .on('error', (err) => {
-            callback(err);
-        });
+    });
+
 };
 
+// -------------------------------------------------------------------------------------------------------------------- //
 
-if (process.argv.length !== 5) {
-    console.log('Usage: node download-jwks.js <AWS region> <Cognito User Pool ID> <output file>');
-    process.exit(1);
-}
+const parser = new ArgumentParser( { addHelp: true } );
+parser.addArgument(['-r', '--region'], { dest: 'region', required: true, metavar: '<AWS region>', help: 'AWS region' });
+parser.addArgument(['-u', '--user-pool-id'], { dest: 'userPoolId', required: true, metavar: '<Cognito user pool id>', help: 'Cognito user pool id' });
 
-const region = process.argv[2];
-const userPoolId = process.argv[3];
-const keyFile = process.argv[4];
+const args = parser.parseArgs();
+const outputFile = args.outputFile || args.userPoolId + '.keys';
 
-downloadKeys(region, userPoolId, (err, keys) => {
-    if (err != null) {
-       console.log(err);
-    }
-    else {
-        console.log(`Saving keys to ${keyFile}`);
-        fs.writeFileSync(keyFile, JSON.stringify(keys));
-    }
-});
+downloadKeys(args.region, args.userPoolId)
+    .then(keys => {
+        console.log(`Saving keys to ${outputFile}`);
+        fs.writeFileSync(outputFile, JSON.stringify(keys, null, 2));
+    })
+    .catch(err => console.log(err));
